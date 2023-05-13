5df7d94424a2d79a89e959cd02f81c6784ed539a
nabildroid <pni20156789@gmail.com>
2023-05-13 22:13:19 +0100

    update design

--- index.php
+++ index.php
@@ -50,16 +50,53 @@ if (isset($_POST['schedule'])) {
 </head>
 
 <body dir="trl">
-    <div class="container max-w-2xl mx-auto mt-4">
-        <h1 class="title max-w-lg mx-auto">أداة فحص شهادة SSL</h1>
-        <form id="check-form" method="POST" action="./">
+
+    <div class="w-full z-50 backdrop-blur-md  bg-gray-300/75 sticky top-0 shadow overflow-hidden">
+        <div class="max-w-2xl mx-auto flex justify-between items-center px-2 sm:px-0 ">
+
+            <div class="flex items-center space-x-4 py-2">
+                <a class="px-2 py-1 rounded-md hover:bg-red-300 block text-lg text-red-500" href="#">Home</a>
+                <a class="px-2 py-1 rounded-md hover:bg-red-300 block text-lg text-red-500" href="#tool">the tool</a>
+                <a class="px-2 py-1 rounded-md hover:bg-red-300 text-lg hidden sm:block text-red-500" href="#about">about tool</a>
+            </div>
+
+
+            <span>Your Name</span>
+
+        </div>
+
+    </div>
+
+
+    <div class="min-h-screen flex flex-col justify-center items-stretch w-full overflow-hidden px-2 sm:px-0">
+        <div class="max-w-4xl mx-auto rounded-lg bg-white shadow-lg my-8 sm:hover:scale-110 transform duration-300 ">
+            <div class="max-w-2xl mx-auto  p-4 text-center">
+                <h1 class="text-3xl py-8 text-red-500">Welcome to this Tool</h1>
+
+                <p class="text-lg leading-8">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <div id="tool" class="h-24"></div>
+    <div class="container max-w-2xl mx-auto mt-4 overflow-hidden">
+        <form   id="check-form" method="POST" action="./#tool">
             <label for="url-input">أدخل رابط الموقع</label>
             <input type="text" id="url-input" name="url" required>
             <div class="flex items-center justify-center space-x-7">
-                <input type="submit" name="ssl" value="ssl فحص">
-                <input type="submit" name="ports" value="فحص ports">
+                <input class="bg-red-500" type="submit" name="ssl" value="ssl فحص">
+                <input id="tool" class="bg-red-500" type="submit" name="ports" value="فحص ports">
             </div>
         </form>
+
 
         <?php if (isset($ports)) { ?>
             <div class="mt-8">
@@ -78,7 +115,7 @@ if (isset($_POST['schedule'])) {
                                 <td class="px-4 py-2"><?php echo $port ?></td>
 
                                 <?php if ($status == "open") { ?>
-                                    <td class="px-4   "><span class="py-2 px-2 before:transform before:-skew-y-6 text-white  relative before:z-0 before:absolute before:inset-0 before:content-[''] before:bg-green-500"><span class="relative z-30"><?php echo $status ?></span></span></td>
+                                    <td class="px-4   "><span class="py-2 px-2 before:transform before:-skew-y-6 text-white  relative before:z-0 before:absolute before:inset-0 before:content-[''] before:bg-red-500"><span class="relative z-30"><?php echo $status ?></span></span></td>
                                 <?php } else { ?>
                                     <td class="px-4 py-4"><?= $status ?></td>
                                 <?php } ?>
@@ -143,6 +180,26 @@ if (isset($_POST['schedule'])) {
 
             </div>
         <?php } ?>
+
+
+
+    </div>
+
+    <div class="min-h-screen flex flex-col justify-center  w-full px-2 sm:px-0 overflow-hidden">
+        <div id="about" class="max-w-4xl mx-auto rounded-lg bg-white shadow-lg my-8 sm:hover:scale-110 transform duration-300 ">
+            <div class="max-w-2xl mx-auto  p-4 text-center">
+                <h1 class="text-3xl py-8 text-red-500">Welcome to this Tool</h1>
+
+                <p class="text-lg leading-8">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates tempora nisi, alias reprehenderit eveniet delectus accusamus debitis ex vitae cum architecto accusantium iure, non impedit nulla assumenda laudantium doloremque laboriosam.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 
 </html>
